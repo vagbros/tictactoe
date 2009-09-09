7a689f88580737bd7583caea9d40152efd6e5ff3
davide <davide@wintermute.unipi.it>
2009-09-09 17:07:06 +0200

    Added README

---
+++ Debug/src/tictactoe.d
@@ -0,0 +1,1 @@
+src/tictactoe.d src/tictactoe.o: ../src/tictactoe.cpp
---
+++ src/tictactoe.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tictactoe.cpp
+// Author      : vagbros
+// Version     :
+// Copyright   : NO COPYRIGHT (at least for now!)
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "E questo e' solo l'inizio..." << endl;
+	return 0;
+}
